a3c9525f2c315f1281e1f53d511ab7e6f3ff3ff3
fangxiao <532696537@qq.com>
2015-12-25 08:54:07 +0800

    add some files

---
+++ My_eps_to_pdf.bat
@@ -0,0 +1,12 @@
+::epstopdf --outfile=hitTime_Bin_1ns-eps-converted-to.pdf hitTime_Bin_1ns.eps
+::epstopdf --outfile=Noise_rate_50kHz_300ns-eps-converted-to.pdf Noise_rate_50kHz_300ns.eps
+::epstopdf --outfile=Noise_rate_50kHz_50ns-eps-converted-to.pdf Noise_rate_50kHz_50ns.eps
+
+
+::epstopdf --outfile=4MeV_e+_PMT_first_hitTime_all-eps-converted-to.pdf 4MeV_e+_PMT_first_hitTime_all.eps 
+::
+::epstopdf --outfile=Rate_all-eps-converted-to.pdf Rate_all.eps
+epstopdf --outfile=C14_deltaR_new-eps-converted-to.pdf C14_deltaR_new.eps 
+
+
+
